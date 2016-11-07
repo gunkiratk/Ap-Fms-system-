1637b5de850158166a06e0d5d5ff1a21d9cde8cd
shaan15 <shaan15090@iiitd.ac.in>
2016-11-07 23:53:57 +0530

    User made

---
+++ User.java
@@ -0,0 +1,18 @@
+class User{
+	private int id;
+	private String username;
+
+	public int getId(){
+		return id;
+	}
+	public String getUsername(){
+		return username;
+	}
+
+	public void setId(int id){
+		this.id = id;
+	}
+	public void setUsername(String u){
+		this.username = u;
+	}
+}
