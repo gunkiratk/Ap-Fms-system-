92f6e10070c3a37aaeb3864ac8ef0e0faa96a038
gunkiratk <gunkirat15032@iiitd.ac.in>
2016-11-11 22:34:58 +0530

    admin and department updated

--- Admin.java
+++ Admin.java
@@ -1,3 +1,42 @@
-class Admin{
-	
+public class Admin extends User{
+	Admin()
+	{
+
+	}
+	public void add(Supervisor s)
+	{
+
+	}
+	public void add(Staff s)
+	{
+
+	}
+	public void view(Staff s)
+	{
+
+	}
+	public void view(Supervisor s)
+	{
+
+	}
+	public void delete(Supervisor s)
+	{
+
+	}
+	public void delete(Staff s)
+	{
+
+	}
+	public void assignTask()
+	{
+
+	}
+	public void approveLog()
+	{
+
+	}
+	public void rejectLog()
+	{
+		
+	}
 }
