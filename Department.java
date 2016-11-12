7e1189f5758b70be35663a3fa8472faf70b7813e
gunkiratk <gunkirat15032@iiitd.ac.in>
2016-11-12 23:49:36 +0530

    Department updated

--- Department.java
+++ Department.java
@@ -1,4 +1,18 @@
 class Department{
+	List<Staff> Staffers=new List<Staff>();
+	Supervisor sv= new Supervisor;
+	Department()
+	{
+
+	}
+	public List<Staff> getStaffers()
+	{
+
+	}
 	
+	public void setStaffers(List<Staff> s)
+	{
+		
+	}
 	
 }
